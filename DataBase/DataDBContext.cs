2bc6278b6b9c7236df1c5ae113279a6c4d1be785
Ivan <van.koshkin.fr@gmail.com>
2021-11-16 09:06:22 +0300

    Add Entity Framework

---
+++ DataBase/DataDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_SQLite_WPF
+{
+    public class DataDBContext : DbContext
+    {
+        public DbSet<Person> PersonData { get; set; }
+        public DataDBContext(DbContextOptions<DataDBContext> options) : base(options) { }
+
+    }
+}
---
+++ Interfaces/IPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_SQLite_WPF
+{
+    public interface IPerson
+    {
+        Task<bool> HasData();
+        Task EnsurePersonStore();
+        Task<List<Person>> GetPersons();
+        Task AddPerson(Person person);
+        Task RemovePerson(Person person);
+        Task UpdatePerson(Person person);
+        Task SavePerson(Person person);
+    }
+}
